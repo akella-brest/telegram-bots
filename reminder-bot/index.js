59d010c376c9ce030ba1842178d34e12e4e148bc
Artsiom Pas <akella1997@tut.by>
2018-02-01 01:34:07 +0300

    init

--- reminder-bot/index.js
+++ reminder-bot/index.js
@@ -10,7 +10,9 @@ bot.onText(/\/start/, function (msg, match) {
     bot.sendMessage(msg.from.id, `${msg.from.first_name}, привет, я бот-напоминальщик! Вводи в формате "Напомни что-то в 23:55" и я тебе напомню`);
 });
 
-bot.onText(/Напомни (.+) в (.+)/, function (msg, match) {
+var reg = new RegExp('Напомни', 'i');
+
+bot.onText(reg, function (msg, match) {
     var userId = msg.from.id;
     var text = match[1];
     var time = match[2];
@@ -22,8 +24,20 @@ bot.onText(/Напомни (.+) в (.+)/, function (msg, match) {
 
 setInterval (function() {
     for(var i = 0; i < notes.length; i++) {
-        var curDate = (new Date().getHours() + 3) + ':' + new Date().getMinutes();
-        if(notes[i]['time'] == curDate){
+        var hours = new Date().getUTCHours() + 3;
+
+        if (hours >= 24) {
+            hours -= 24;
+        }
+
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+
+        var curDateDot = hours + '.' + new Date().getMinutes();
+        var curDate = hours + ':' + new Date().getMinutes();
+
+        if(notes[i]['time'] == curDate || notes[i]['time'] == curDateDot){
             bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
             notes.splice(i, 1);
         }
