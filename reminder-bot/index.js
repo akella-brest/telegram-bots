eabc339ed7a78ca4a1e7c6f8c758ba290c92ca74
Artsiom Pas <akella1997@tut.by>
2018-01-31 16:25:19 +0300

    init

--- reminder-bot/index.js
+++ reminder-bot/index.js
@@ -10,12 +10,12 @@ var bot = new TelegramBot(token, {polling: true});
 var notes = [];
 
 bot.onText(/\/start/, function (msg, match) {
-    bot.sendMessage(msg.from.id, `${msg.from.first_name}, привет, я бот-напоминальщик! Вводи в формате "Напомни что-то в 23:55" и я тебе напомню`);
+    bot.sendMessage(msg.from.id, `${msg.from.first_name}, привет, я бот-напоминальщик! Вводи в формате "/Напомни что-то в 23:55" и я тебе напомню`);
 });
 
 
 
-bot.onText(/\Напомни (.+) в (.+)/, function (msg, match) {
+bot.onText(/\/Напомни (.+) в (.+)/, function (msg, match) {
     var userId = msg.from.id;
     var text = match[1];
     var time = match[2];
@@ -35,9 +35,9 @@ bot.onText(/\Напомни (.+) в (.+)/, function (msg, match) {
 setInterval (function() {
     for(var i = 0; i < notes.length; i++) {
         var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-            if(notes[i]['time'] == curDate){
-                bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
+        if(notes[i]['time'] === curDate){
+            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
             notes.splice(i, 1);
-            }
         }
-    }, 1000);
+    }
+}, 1000);
