88171d8018e09dd35e7a72f39470730490452068
Artsiom Pas <akella1997@tut.by>
2018-02-01 00:53:21 +0300

    init

--- reminder-bot/index.js
+++ reminder-bot/index.js
@@ -1,10 +1,7 @@
-//@ReminderTelegramBot
-
 var TelegramBot = require('../node_modules/node-telegram-bot-api');
 
-// Устанавливаем токен, который выдавал нам бот.
 var token = '453381712:AAHqB0XPmLMkzigVSAqguHUrEJN2aZHGQXM';
-// Включить опрос сервера
+
 var bot = new TelegramBot(token, {polling: true});
 
 var notes = [];
@@ -12,8 +9,6 @@ var notes = [];
 bot.onText(/\/start/, function (msg, match) {
     bot.sendMessage(msg.from.id, `${msg.from.first_name}, привет, я бот-напоминальщик! Вводи в формате "/Напомни что-то в 23:55" и я тебе напомню`);
 });
-
-
 
 bot.onText(/Напомни (.+) в (.+)/, function (msg, match) {
     var userId = msg.from.id;
@@ -25,13 +20,6 @@ bot.onText(/Напомни (.+) в (.+)/, function (msg, match) {
     bot.sendMessage(userId, 'Отлично! Я обязательно постараюсь напомнить :)');
 });
 
-// bot.onText(/(.+)/, function (msg, match) {
-//     bot.sendMessage(msg.from.id, `${msg.from.first_name}, я не понимать тебя :) Вводи в формате "/напомни что-то в 23:55"`);
-// });
-
-// bot.on('message', msg => {
-//     bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`)
-// })
 setInterval (function() {
     for(var i = 0; i < notes.length; i++) {
         var curDate = new Date().getHours() + ':' + new Date().getMinutes();
